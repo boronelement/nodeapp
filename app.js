4eeb6b07725aa88c3deb1d025562fb235725c2a8
ubuntu <ubuntu@ub-server>
2023-05-17 22:34:05 +0000

    app.js updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World2');
+  res.end('Hello World changed code live at 4:03am');
 });
 
 server.listen(3000);
