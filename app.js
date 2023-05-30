fe46f9390a76bf59e11214563388f4caa8046a64
ganeshjega <c.ganesh.j@levadata.com>
2023-05-30 01:57:44 -0700

    Added current timestamp to the console message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World changed code live at 4:03am');
+  res.end('Hello World changed code live at 11:42am. The current time is ' + new Date(Date.now()));
 });
 
 server.listen(3000);
